886f07428ca1a80a0e51f86859bbfd7bdb9db0c0
wavekanit <wavekanit@hotmail.com>
2025-01-29 11:03:07 +0700

    feat: eslint prettier setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,77 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
+import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    { ignores: ['dist'] },
+    {
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommended,
+            prettierConfig,
+        ],
+        files: ['**/*.{ts,tsx,js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            'unused-imports': unusedImports,
+            prettier,
+            react,
+            import: importPlugin,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            'prettier/prettier': [
+                'warn',
+                {
+                    endOfLine: 'auto',
+                },
+            ],
+            semi: ['warn', 'always'],
+            'arrow-body-style': ['warn', 'as-needed'],
+            'react/jsx-curly-brace-presence': [
+                'error',
+                { props: 'never', children: 'never' },
+            ],
+            'unused-imports/no-unused-imports': 'warn',
+            'padding-line-between-statements': [
+                'warn',
+                { blankLine: 'always', prev: '*', next: 'return' },
+            ],
+            'import/order': [
+                'warn',
+                {
+                    groups: [
+                        'builtin',
+                        'external',
+                        'internal',
+                        'parent',
+                        'sibling',
+                        'index',
+                    ],
+                    'newlines-between': 'never',
+                },
+            ],
+            'padding-line-between-statements': [
+                'warn',
+                { blankLine: 'always', prev: 'import', next: '*' },
+                { blankLine: 'never', prev: 'import', next: 'import' },
+            ],
+        },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+);
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+export default {
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    useTabs: false,
+    bracketSpacing: true,
+    arrowParens: 'always',
+};
